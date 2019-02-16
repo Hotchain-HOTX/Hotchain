7889b2ab9c65962558b1aadb217b61eaef4bd624
hotadmin <43881205+hotadmin@users.noreply.github.com>
2019-02-17 00:22:53 +0100

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -127,8 +127,8 @@ public:
         nMaxMoneyOut = 60000000 * COIN;
 
 	nZerocoinStartTime = 1547814260;
-        nZerocoinStartHeight = 500;
-        nBlockZerocoinV2 = 501; //!> The block that zerocoin v2 becomes active - roughly Tuesday, May 8, 2018 4:00:00 AM GMT
+        nZerocoinStartHeight = 400;
+        // nBlockZerocoinV2 = 80001; //!> The block that zerocoin v2 becomes active - roughly Tuesday, May 8, 2018 4:00:00 AM GMT
         /** Height or Time Based Activations **/
         nLastPOWBlock = 200;
        /* nModifierUpdateBlock = 615800;
