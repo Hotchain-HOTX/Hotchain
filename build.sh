c3db4cf281a5de0887ba31a4a30853769b433f01
hotchaindev <hotdev@hotchain.me>
2019-03-03 13:18:31 +0100

    disable tests to simplify cache

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 ./autogen.sh
 cd depends && make $MAKEJOBS $OPT
-cd $TRAVIS_BUILD_DIR && ./configure --prefix=$TRAVIS_BUILD_DIR/depends/$HOST $CONF
+cd $TRAVIS_BUILD_DIR && ./configure --disable-tests --disable-gui-tests --disable-bench --prefix=$TRAVIS_BUILD_DIR/depends/$HOST $CONF
 make $MAKEJOBS $GOAL
 echo "all done"
 exit 0
